74a156728e3bc9e7fd13ad738ac47458c55ffc43
Rizvi Rahman Omee <rizvi_rahman_01@yahoo.com>
2022-02-03 00:02:13 +0600

    Structure building 2nd day

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const { urlencoded } = require("body-parser");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+// const { urlencoded } = require("body-parser");
 
 
 dotenv.config();
@@ -24,5 +26,23 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine","ejs");
 
+app.use(express.static(path.join(__dirname,"public")));
+
+
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+
+// ROUTING SETUP
+
+
+
+
+// ERROR HANDLING
+
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Application is running at http://localhost:${process.env.PORT}`);
+});
 
 // console.log(process.env.APP_NAME);
