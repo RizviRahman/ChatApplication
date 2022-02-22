496e33f2973db70c4b927951e3e898ef6fcc7615
Rizvi Rahman Omee <rizvi_rahman_01@yahoo.com>
2022-02-22 16:07:31 +0600

    22.02.2022 office

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -5,10 +5,14 @@ const express = require("express");
 // internal imports
 const { getUsers } = require("../controllers/usersController");
 
+const avatarUpload = require("../middlewares/users/avatarUpload");
+
 const router = express.Router();
 
 
 // User page
 router.get("/", getUsers);
+
+router.post("/", avatarUpload);
 
 module.exports = router;
